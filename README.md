97d9e0662469ac97bac0da9f872ec4455f9033ce
Matraf <sanandreas180@wp.pl>
2022-01-16 09:58:30 +0100

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Testowanie UI za pomocą Selenide
 **Projekt na przedmiot "Wstęp do testowania" 2021/2022**
+
+# Zakres testów
+- test wyszukiwarki adresów na terenie miasta Poznań
+- test wyszukiwarki strony
+- test odnośników znajdujących się na stronie
+- test opcji zgłaszania interwencji do Straży Miejskiej
+- test menu strony
--- src/test/java/PoznanSiteTest.java
+++ src/test/java/PoznanSiteTest.java
@@ -1,15 +1,11 @@
-import static com.codeborne.selenide.Condition.id;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static junit.framework.TestCase.assertEquals;
 
 import com.codeborne.selenide.*;
-import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PoznanSiteTest {
@@ -31,11 +27,12 @@ public class PoznanSiteTest {
         String postalCode = $(By.xpath("/html/body/div[2]/div[2]/div[1]/div[6]/div/div[1]/div/table[1]/tbody/tr[1]/td[2]")).getText();
         String localGovernment = $(By.xpath("/html/body/div[2]/div[2]/div[1]/div[6]/div/div[1]/div/table[1]/tbody/tr[3]/td[2]/div/div/a")).getText();
         String electoralCircuit = $(By.xpath("/html/body/div[2]/div[2]/div[1]/div[6]/div/div[1]/div/table[1]/tbody/tr[2]/td[2]/ul/li[1]/a")).getText();
-        closeWindow();
 
         Assertions.assertEquals(postalCode, "61-614");
         Assertions.assertEquals(localGovernment, "Morasko-Radojewo");
         Assertions.assertEquals(electoralCircuit, "227");
+
+        closeWindow();
     }
 
     @Test
@@ -47,7 +44,9 @@ public class PoznanSiteTest {
         $(By.xpath("/html/body/div[1]/header/div/nav/div[1]/div[5]/form/fieldset/input")).click();
         $(byName("string")).setValue("auto").pressEnter();
         $(byId("search_counter")).equals("Znaleziono 867 wyników");
-        $(By.xpath("/html/body/section/div/div/div[2]/div/div/form/div[2]/fieldset/div[2]/div[1]/span[2]/a")).click();
+        $(By.xpath("/html/body/section/div/div/div[2]/div/div/form/div[2]/fieldset/div[2]/div[1]/span[1]"))
+                .should(Condition.text("Filtry"))
+                .click();
         $(byId("criteria_obiekty")).click();
         $(byId("criteria_klasy")).click();
         $(byId("criteria_wiadmag")).click();
@@ -67,12 +66,15 @@ public class PoznanSiteTest {
         open("https://www.poznan.pl/");
         $(byId("cookies-button-ok")).click();
 
-        $(By.xpath("/html/body/section/div/div/section[5]/div/div[4]/section[1]/div/div[2]/div/article[1]/a")).click();
-        String text = $(By.xpath("/html/body/div[3]/div/div/div/article/ul/li[1]/p/a[1]")).getText();
-        closeWindow();
+        $(By.xpath("/html/body/section/div/div/section[5]/div/div[4]/section[1]/div/div[2]/div/article[1]/a"))
+                .should(Condition.text("Prezydent Miasta i Zastępcy"))
+                .click();
 
+        String text = $(By.xpath("/html/body/div[3]/div/div/div/article/ul/li[1]/p/a[1]")).getText();
         String[] splitText = text.split(" ");
         assertEquals(splitText[splitText.length-2] + " " + splitText[splitText.length-1], "Jacek Jaśkowiak");
+
+        closeWindow();
     }
 
     @Test
@@ -80,23 +82,39 @@ public class PoznanSiteTest {
         open("https://www.poznan.pl/");
         $(byId("cookies-button-ok")).click();
 
-        $(By.xpath("/html/body/section/div/div/section[5]/div/div[3]/section[1]/div/article/p[2]/a")).click();
+        $(By.xpath("/html/body/section/div/div/section[5]/div/div[3]/section[1]/div/article/p[2]/a"))
+                .should(Condition.text("Zgłoś interwencję"))
+                .click();
+
         $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form/div[2]/div[2]/div/fieldset/label")).click();
         $(By.id("uz_kategoria")).click();
         $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form/fieldset[1]/div/div[2]/select/optgroup[8]/option[9]")).click();
         $(By.id("uz_temat")).setValue("Testowe zgłoszenie");
         $(By.id("uz_tresc")).setValue("Testowe zgłoszenie");
+
+        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form/fieldset[3]/div/div/div/button"))
+                .should(Condition.text("Wskaż na planie"))
+                .click();
+        $(By.id("id_ulica_street_lookup")).setValue("Uniwersytetu Poznańskiego 4");
+        $(By.id("id_ulica_street_submit"))
+                .should(Condition.text("Wyszukaj"))
+                .click();
+        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/div[2]/button"))
+                .should(Condition.text("Zatwierdź"))
+                .click();
+
         $(By.id("uz_email")).setValue("Testowe@zgłoszenie.com");
 
-        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form/fieldset[3]/div/div/div/button")).click();
-        $(By.id("id_ulica_street_lookup")).setValue("Uniwersytetu Poznańskiego 4");
-        $(By.id("id_ulica_street_submit")).click();
-        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/div[2]/button")).click();
-        $(By.name("goto_summary")).click();
+        $(By.name("goto_summary"))
+                .should(Condition.text("Dalej >>"))
+                .click();
 
-        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/fieldset[2]/div[1]/div[2]/p")).shouldHave(Condition.text("Testowe zgłoszenie"));
-        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/fieldset[2]/div[2]/div[2]/p")).shouldHave(Condition.text("Testowe zgłoszenie"));
-        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/fieldset[5]/div/div[2]/p")).shouldHave(Condition.text("Testowe@zgłoszenie.com"));
+        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/fieldset[2]/div[1]/div[2]/p"))
+                .shouldHave(Condition.text("Testowe zgłoszenie"));
+        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/fieldset[2]/div[2]/div[2]/p"))
+                .shouldHave(Condition.text("Testowe zgłoszenie"));
+        $(By.xpath("/html/body/section[2]/div/div[2]/article/div/div/form[1]/fieldset[5]/div/div[2]/p"))
+                .shouldHave(Condition.text("Testowe@zgłoszenie.com"));
 
         closeWindow();
     }
@@ -106,16 +124,27 @@ public class PoznanSiteTest {
         open("https://www.poznan.pl/");
         $(byId("cookies-button-ok")).click();
 
-        $(By.xpath("/html/body/div[1]/header/div/nav/div[1]/div[1]/a[2]")).click();
-        $(By.xpath("/html/body/div[1]/header/div/nav/div[1]/div[1]/div/div/ul[2]/li[7]/a")).click();
-        $(By.xpath("/html/body/div[1]/header/div/nav/div[1]/div[1]/div/div/ul[2]/li[7]/ul/li[6]/a")).should(Condition.text("Kuchnia wielkopolska")).click();
-        $(By.xpath("/html/body/section/div/div/section[3]/div/div/section/div/div[2]/div[3]/article/div/h3/a")).click();
-        $(By.xpath("/html/body/section[2]/div/div[2]/article/div[2]/div/div[4]/div/p[8]/a")).click();
+        $(By.xpath("/html/body/div[1]/header/div/nav/div[1]/div[1]/a[2]"))
+                .should(Condition.text("Dla mieszkańca"))
+                .click();
+        $(By.xpath("/html/body/div[1]/header/div/nav/div[1]/div[1]/div/div/ul[2]/li[7]/a"))
+                .should(Condition.text("Tożsamość, historia, cmentarze"))
+                .click();
+        $(By.xpath("/html/body/div[1]/header/div/nav/div[1]/div[1]/div/div/ul[2]/li[7]/ul/li[6]/a"))
+                .should(Condition.text("Kuchnia wielkopolska"))
+                .click();
+        $(By.xpath("/html/body/section/div/div/section[3]/div/div/section/div/div[2]/div[3]/article/div/h3/a"))
+                .should(Condition.text("Mięsa"))
+                .click();
+        $(By.xpath("/html/body/section[2]/div/div[2]/article/div[2]/div/div[4]/div/p[8]/a"))
+                .should(Condition.text("Bigos"))
+                .click();
         ElementsCollection selenideElements = $$(By.xpath("/html/body/section[2]/div/div[2]/article/div[2]/div/div[2]/div/ul"));
 
-//        ArrayList<String> ingredients = new ArrayList<>();
-//        List<String> strings = selenideElements.texts().stream().toList();
-
+        List<String> strings = selenideElements.texts().stream().toList();
+        String ingredientsInOneString = strings.get(0);
+        String[] split = ingredientsInOneString.split("\n");
+        assertEquals(split.length, 14);
 
         closeWindow();
     }
